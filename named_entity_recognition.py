d9af257b1d07c853f65432c817aa91cd763ffb9a
AdiKrasin <adikrasin@gmail.com>
2020-01-09 14:15:00 +0200

    added feature extraction

--- named_entity_recognition.py
+++ named_entity_recognition.py
@@ -1,9 +1,88 @@
 from nltk.corpus import conll2002
 
-etr = conll2002.chunked_sents('esp.train') # In Spanish
-eta = conll2002.chunked_sents('esp.testa') # In Spanish
-etb = conll2002.chunked_sents('esp.testb') # In Spanish
 
-dtr = conll2002.chunked_sents('ned.train') # In Dutch
-dta = conll2002.chunked_sents('ned.testa') # In Dutch
-dtb = conll2002.chunked_sents('ned.testb') # In Dutch
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+    features = [
+        'bias',
+        'word.lower=' + word.lower(),
+        'word[-3:]=' + word[-3:],
+        'word[-2:]=' + word[-2:],
+        'word[-1:]=' + word[-1:],
+        'word[:1]=' + word[:1],
+        'word[:2]=' + word[:2],
+        'word[:3]=' + word[:3],
+        'word.isupper=%s' % word.isupper(),
+        'word.isdigit=%s' % word.isdigit(),
+        'postag=' + postag,
+        'postag[:2]=' + postag[:2],
+    ]
+    if i > 0:
+        word1 = sent[i - 1][0]
+        postag1 = sent[i - 1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:postag=' + postag1,
+            '-1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('BOS')
+
+    if i < len(sent) - 1:
+        word1 = sent[i + 1][0]
+        postag1 = sent[i + 1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:postag=' + postag1,
+            '+1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('EOS')
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+
+etr = conll2002.iob_sents('esp.train') # In Spanish
+eta = conll2002.iob_sents('esp.testa') # In Spanish
+etb = conll2002.iob_sents('esp.testb') # In Spanish
+
+dtr = conll2002.iob_sents('ned.train') # In Dutch
+dta = conll2002.iob_sents('ned.testa') # In Dutch
+dtb = conll2002.iob_sents('ned.testb') # In Dutch
+
+train_sents = list(etr)
+test_sents = list(eta)
+
+X_train = [sent2features(s) for s in train_sents]
+y_train = [sent2labels(s) for s in train_sents]
+
+X_test = [sent2features(s) for s in test_sents]
+y_test = [sent2labels(s) for s in test_sents]
