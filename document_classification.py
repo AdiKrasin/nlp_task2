44bd5d999dd2abf423c68a91f90b7d91e66a27de
AdiKrasin <adikrasin@gmail.com>
2020-01-11 20:20:14 +0200

    document classification statistic exploration added

--- document_classification.py
+++ document_classification.py
@@ -6,15 +6,6 @@ def collection_stats():
     # List of documents
     documents = reuters.fileids()
     print(str(len(documents)) + " documents")
-    '''
-    train_docs = list(filter(lambda doc: doc.startswith("train"),
-                             documents))
-    print(str(len(train_docs)) + " total train documents")
-
-    test_docs = list(filter(lambda doc: doc.startswith("test"),
-                            documents))
-    print(str(len(test_docs)) + " total test documents")
-    '''
 
     # List of categories
     categories = reuters.categories()
@@ -23,7 +14,6 @@ def collection_stats():
     docs_per_category = list()
     # Documents in a category
     for category in categories:
-        #category_docs = reuters.fileids("acq")
         category_docs = reuters.fileids(category)
         print('for the following category: {} this is the amount of docs: {}'.format(category, len(category_docs)))
         docs_per_category.append(len(category_docs))
@@ -33,14 +23,27 @@ def collection_stats():
     print('this is the min docs per category: {}'.format(min(docs_per_category)))
     print('this is the max docs per category: {}'.format(max(docs_per_category)))
 
-'''
-    # Words for a document
-    document_id = category_docs[0]
-    document_words = reuters.words(category_docs[0])
-    print(document_words)
 
-    # Raw document
-    print(reuters.raw(document_id))
+def additional_stats():
+    documents = reuters.fileids()
+    categories = reuters.categories()
+    total_amount_of_documents_words = 0
+    total_amount_of_document_characters = 0
+    for category in categories:
+        category_docs = reuters.fileids(category)
+        # Words for a document
+        document_id = category_docs[0]
+        document_words = reuters.words(category_docs[0])
+        total_amount_of_documents_words += len(document_words)
+        for word in document_words:
+            total_amount_of_document_characters += len(word)
+    print('total amount of documents words: {}'.format(total_amount_of_documents_words))
+    print('total amount of documents characters: {}'.format(total_amount_of_document_characters))
 
 '''
+# this is just for 2.1.1
 collection_stats()
+'''
+
+# this is just for 2.1.2
+additional_stats()
